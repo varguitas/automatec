62454cdbb300ecf952c3b2d979d7e34123672238
yulianup <yulianup93@gmail.com>
2015-06-02 09:28:09 -0600

    Modificar Modelo, Controlador y Rutas de Question

--- app/routes/questions.server.routes.js
+++ app/routes/questions.server.routes.js
@@ -6,12 +6,12 @@ module.exports = function(app) {
 
 	// Questions Routes
 	app.route('/questions')
-		.get(users.requiresLogin, questions.isTeacher, questions.list)
-		.post(users.requiresLogin, questions.isTeacher, questions.create);
+		.get(users.requiresLogin, users.hasAuthorization(['teacher','admin']), questions.list)
+		.post(users.requiresLogin, users.hasAuthorization(['teacher','admin']), questions.create);
 
 	app.route('/questions/:questionId')
-		.get(users.requiresLogin, questions.isTeacher, questions.read)
-		.put(users.requiresLogin, questions.isTeacher, questions.hasAuthorization, questions.update)
+		.get(users.requiresLogin, users.hasAuthorization(['teacher','admin']), questions.read)
+		.put(users.requiresLogin, users.hasAuthorization(['teacher','admin']), questions.hasAuthorization, questions.update)
 		.delete(users.requiresLogin, questions.isTeacher, questions.hasAuthorization, questions.delete);
 
 	// Finish by binding the Question middleware
