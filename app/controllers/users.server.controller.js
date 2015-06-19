09fd7639dc824fcd5a77bbde27610844673009de
josacord92@gmail.com <josacord92@gmail.com>
2015-06-19 16:22:54 -0600

    Revert "Trabajado: Jose Carlos Alvarez Umaña, Boris Beck, Luis Diego Fonseca. Se inician las funciones addRole(userOid,role), revmoveRole(UserOid,role), getRoles(userOid), getAllUsersRoles"
    This reverts commit 8a064b8f1613365c17b97fb8710652c2d8f7eec6.

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -14,8 +14,3 @@ module.exports = _.extend(
 	require('./users/users.password.server.controller'),
 	require('./users/users.profile.server.controller')
 );
-
-exports.addRole = function(userOid,role){};
-exports.removeRole = function(userOid,role){};
-exports.getRoles = function(userOid){};
-exports.getAllUserRoles = function(){};
