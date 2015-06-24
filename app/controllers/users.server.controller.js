a18126492d8345dc03816d4a10be4e9a49225a83
josacord92@gmail.com <josacord92@gmail.com>
2015-06-23 20:41:05 -0600

    Jose Carlos Álvarez, Boris Beck, Luis Diego Fonseca.  Funciones addRole, removeRole, getRoles, getAllUsersRoles

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -14,3 +14,21 @@ module.exports = _.extend(
 	require('./users/users.password.server.controller'),
 	require('./users/users.profile.server.controller')
 );
+
+user.server.controller.prototype ={
+	addRole: function(req,res){
+	var id = req.body.userOid;
+	var role = req.body.role;
+	}
+	
+	removeRole: function(req,res){
+		
+	}
+	getRoles: function(req,res){
+	role.find()	
+	}
+	
+	getAllUsersRoles: function(req,res){
+	
+	}
+}
