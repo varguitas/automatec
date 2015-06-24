e5a264ed6d133fff27e29036e47031c70afe4ca6
bbeck189 <bbeck189@users.noreply.github.com>
2015-06-23 22:36:53 -0600

    Update users.server.controller.js

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -3,7 +3,10 @@
 /**
  * Module dependencies.
  */
-var _ = require('lodash');
+var mongoose = require('mongoose'),
+	errorHandler = require('./errors.server.controller'),
+	User = mongoose.model('User'),
+	_ = require('lodash');
 
 /**
  * Extend user's controller
@@ -14,3 +17,33 @@ module.exports = _.extend(
 	require('./users/users.password.server.controller'),
 	require('./users/users.profile.server.controller')
 );
+
+function addRole(userOid, role) {
+	User.update(
+		{ "_id": userOid },
+		{ "$push": { "roles": role } },
+		callback);
+}
+
+function removeRole(userOid, role){
+	User.update(
+		{'_id': userOid}, 
+		{ "$pull": { "roles" : { type: role } } },
+		false,
+		true);
+}
+
+function getRoles(userOid){
+	User.aggregate(
+		[
+			{ $match: { "_id": userOid } },
+			{ $sort: { "_id": -1 } }
+		],
+		callback);
+}
+
+function getAllUsersRoles() {
+	User.find(
+        { },
+		{  "username": 1, "firstName": 1, "lastName": 1 , "roles": 1} );
+}
